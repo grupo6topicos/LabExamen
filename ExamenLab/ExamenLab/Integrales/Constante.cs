19265861bb62012b3fec3449810744db04c5ec84
grupo6topicos <topicos20152grupo6@gmail.com>
2015-07-11 15:12:10 -0600

    PrimerUpdate

---
+++ ExamenLab/ExamenLab/Integrales/Constante.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExamenLab.Integrales
+{
+    public class Constante
+    {
+        double a;
+        double b;
+        double k;
+
+        double solucion;
+
+        public double ValorA
+        {
+            get
+            { return a; }
+            set
+            { a = ValorA; }
+        }
+
+        public double ValorB
+        {
+            get
+            { return b; }
+            set
+            { b = ValorB; }
+        }
+
+        public double ValorK
+        {
+            get
+            { return k; }
+            set
+            { k = ValorK; }
+        }
+
+        public double ValorSolucion { 
+            get
+            { return solucion; }
+            set
+            { solucion = ValorSolucion; }
+        }
+
+        public Constante() {
+            ValorA = 0.0;
+            ValorB = 0.0;
+            ValorK = 0.0;
+        }
+
+        public Constante(double a, double b, double k){
+            ValorA = a;
+            ValorB = b;
+            ValorK = k;
+        }
+
+        public bool VerificarRango() {
+            bool resultado = true;
+            if(b < a)
+                resultado  = false;
+            return resultado;
+
+        }
+
+        private void EncontrarSolucion() { 
+            solucion = (k*(b-a));
+        }
+
+        public void ObtenerSolucion(){
+            if (VerificarRango()) {
+                throw new Exception("El valor [b] debe ser mayor al valor [a]");
+            }
+
+            EncontrarSolucion();
+        }
+    }
+}
---
+++ ExamenLab/WcfService1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WcfService1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("P&G")]
+[assembly: AssemblyProduct("WcfService1")]
+[assembly: AssemblyCopyright("Copyright © P&G 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d48092f3-357f-4eb6-91bd-8c6ede1a9a1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
